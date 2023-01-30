c8ae406ffbb1fca9d780103bdb4875e14586af33
Koryun <saribekyan.koryun@mail.ru>
2023-01-30 18:17:19 +0400

    Кроме доп задачи

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<100)
+{
+        Console.Write("Вы ошиблись!\nВведите трёхзначное число: ");
+}
+else if (a>999)
+{
+        Console.Write("Вы ошиблись!\nВведите трёхзначное число: ");
+}
+else
+{
+int a1 = a/10;
+int a2 = a1%10;
+Console.WriteLine (a2); 
+}
